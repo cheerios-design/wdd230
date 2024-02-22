818f3a71e7b77217d45c9a50eca5ed241e51984f
cheerios-design <samdotmc@gmail.com>
2024-02-23 00:05:16 +0300

    improved last modified date display.

--- chamber/scripts/lastModifiedDate.js
+++ chamber/scripts/lastModifiedDate.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the last modified date of the current page
     var lastModifiedDate = document.lastModified;
 
-    // Display the last modified date in the designated element
-    var lastModifiedElement = document.getElementById('lastModified');
+    // Display the last modified date in the designated element in the footer
+    var lastModifiedElement = document.getElementById('lastmodified');
     lastModifiedElement.textContent = "Last Modified: " + lastModifiedDate;
 });
